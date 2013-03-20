0f3f3bd4596c930295fa4f0da7630b98011863dc
maydt <maydt@exoplatformm.com>
2013-03-20 14:03:05 +0700

    PLFENG-1414: Update properties files after remove _en files from projects

--- translations/src/config/projects.sh
+++ translations/src/config/projects.sh
@@ -1,5 +1,5 @@
-projects=( 'platform' 'ecms' 'forum' 'wiki' 'social' 'commons' 'integration' 'gatein-portal' )
+projects=( 'platform' 'platform-ui' 'ecms' 'calendar' 'forum' 'wiki' 'social' 'commons' 'integration' 'gatein-portal' )
 #'gatein' )
-versions=( '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '3.5.x' )
+versions=( '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '3.5.x' )
 
 echo "WORKING PROJECTS = [${projects[@]}]"
