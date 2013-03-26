4cb3178c1814584f38ee558fe82fe8775ae46e46
maydt <maydt@exoplatformm.com>
2013-03-26 09:31:48 +0700

    PLFENG-1413: CROWDIN - Init the PLF 4 project for an English review

--- translations/src/config/projects.sh
+++ translations/src/config/projects.sh
@@ -1,5 +1,4 @@
 projects=( 'platform' 'platform-ui' 'ecms' 'calendar' 'forum' 'wiki' 'social' 'commons' 'integration' 'gatein-portal' )
-#'gatein' )
 versions=( '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '3.5.x' )
 
 echo "WORKING PROJECTS = [${projects[@]}]"
