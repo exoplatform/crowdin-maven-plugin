7b04a8a1967ec2870ed49e7dd0f54a1752a37224
An Bao NGUYEN <annb@exoplatform.com>
2013-07-22 16:07:02 +0700

    Revert "PLFENG-1805: POC for automation crowdin"
    This reverts commit 5f28e729d8bc37240965241ba9682fddc880cb88.

--- translations/src/scripts/post-process.sh
+++ translations/src/scripts/post-process.sh
@@ -38,53 +38,5 @@ for (( i=0;i<$length;i++)); do
   fi
 done
 
-echo "=========================Pushing to github========================="
-echo ""
-
-## arg1: PLF-XXXX
-## arg2: en,fr (list of languages)
-## arg3: W29
-## Commit message "PLF-XXXX: inject en,fr translation W29"
-##MESSAGE_COMMIT="$arg1: inject $arg2 translation $arg3"
-##echo " This is message commit: $MESSAGE_COMMIT "
-
-##for (( i=0;i<$length;i++)); do
-#  if [ -d $EXO_PROJECTS/${projects[${i}]}]; then    
-#	echo "For ${projects[${i}]}"
-#	git checkout stable/${versions[${i}]}
-#	git remote rm exodev
-#	git remote add exodev git@github.com:exodev/${projects[${i}]}.git       
-#	git fetch exodev
-
-	##for each project 
-#		if [ -n "$(git status --porcelain)" ]; then 
-#		echo "there are changes"; 
-		
-#			if [ $arg2 =="en,fr"]; then
-				##if en,fr is used
-					## Commit message "PLF-XXXX: inject en,fr translation W29"
-#					git add . ; git commit -m $MESSAGE_COMMIT
-#					git checkout -b fix/${versions[${i}]}/PLF-XXXX
-#					git push exodev fix/${versions[${i}]}/PLF-XXXX
-#					echo "Push to exodev"
-#					git checkout stable/${versions[${i}]}
-#			else	
-				##if other language
-#					git branch -D feature/${versions[${i}]}-translation
-					## Commit message "PLF-XXXX: inject en,fr translation W29"
-#					git add . ; git commit -m $MESSAGE_COMMIT				
-#					git checkout -b feature/${versions[${i}]}-translation remotes/exodev/feature/${versions[${i}]}-translation
-#					git cherry-pick HEAD@{1}
-#					echo "Push to feature/${versions[${i}]}-translation"
-#					git push exodev feature/${versions[${i}]}-translation
-#					git checkout stable/${versions[${i}]}					
-#			fi							
-#		else 
-#		  echo "no changes, no commit ";
-#		fi
-#  fi
-#done
-
-
 echo ""
 echo "=========================Finished=============================================="
