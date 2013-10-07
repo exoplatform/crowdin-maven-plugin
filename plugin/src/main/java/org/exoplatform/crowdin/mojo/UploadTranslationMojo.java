85067faee62134dd6490be385c97ca1dde93bb1b
An Bao NGUYEN <annb@exoplatform.com>
2013-10-07 14:01:17 +0700

    PLFENG-1968: fix upload translations

--- plugin/src/main/java/org/exoplatform/crowdin/mojo/UploadTranslationMojo.java
+++ plugin/src/main/java/org/exoplatform/crowdin/mojo/UploadTranslationMojo.java
@@ -19,97 +19,94 @@
 package org.exoplatform.crowdin.mojo;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Properties;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
 import org.exoplatform.crowdin.model.CrowdinFile;
-import org.exoplatform.crowdin.model.CrowdinTranslation;
+import org.exoplatform.crowdin.utils.FileUtils;
 
 @Mojo(name = "upload-translation")
 public class UploadTranslationMojo extends AbstractCrowdinMojo {
-  @Parameter(property = "exo.crowdin.upload-translation.properties", required = true)
-  private String uploadTransPropsFile;
-  private Properties uploadTransMainProps;
-  private HashMap<String, Properties> uploadTransProperties;
 
   @Override
   public void crowdInMojoExecute() throws MojoExecutionException, MojoFailureException {
-    // Initialization of the properties
-    uploadTransMainProps = new Properties();
-    uploadTransProperties = new HashMap<String, Properties>();
-    try {
-      if (getLog().isDebugEnabled()) getLog().debug("*** Loading the main properties file (" + uploadTransPropsFile + ")...");
-      uploadTransMainProps = loadProperties(uploadTransPropsFile);
-      Set<Object> keys = uploadTransMainProps.keySet();
-      for (Object key : keys) {
-        if (getLog().isDebugEnabled()) getLog().debug("*** Loading the properties file (" + uploadTransMainProps.getProperty(key.toString()) + ")...");
-        uploadTransProperties.put(key.toString(), loadProperties(uploadTransMainProps.getProperty(key.toString())));
-      }
-      keys = null;
+    if (isAllPropertyFilesExisted()) {
+      // Iterate on each project defined in upload-translation.properties
+      for (String proj : getProperties().keySet()) {
+        getLog().info("Starting project " + proj);
+        // Get the Properties of the current project, i.e. the content of
+        // cs-2.2.x.properties
+        Properties currentProj = getProperties().get(proj);
+        String baseDir = currentProj.getProperty("baseDir");
+        Set<Object> keys = currentProj.keySet();
+        // Iterate on each file of the current project
+        for (Object key : keys) {
+          // Skip the property baseDir
+          if (key.equals("baseDir"))
+            continue;
+          // Construct the full path to the file
+          String filePath = getWorkingDir() + File.separator + proj + File.separator
+              + currentProj.getProperty(key.toString());
 
-    } catch (IOException e) {
-      getLog().error("Could not load the properties. Exception: " + e.getMessage());
-      if (getLog().isDebugEnabled()) {
-        for (StackTraceElement elt : e.getStackTrace()) {
-          getLog().debug("*** " + elt.toString());
+          CrowdinFile master = getFactory().prepareCrowdinFile(filePath, key.toString(), baseDir);
+          uploadTranslation(master);
         }
+        getLog().info("Finished project " + proj);
       }
-      throw new MojoExecutionException("Could not load the properties. Exception: " + e.getMessage());
     }
 
-    // Iterate on each project defined in upload-translation.properties
-    for (String proj : uploadTransProperties.keySet()) {
-      getLog().info("Starting project " + proj);
-      // Get the Properties of the current project, i.e. the content of cs-2.2.x.properties
-      Properties currentProj = uploadTransProperties.get(proj);
-      String baseDir = currentProj.getProperty("baseDir");
-      Set<Object> keys = currentProj.keySet();
-      // Iterate on each file of the current project
-      for (Object key : keys) {
-        // Skip the property baseDir
-        if (key.equals("baseDir")) continue;
-        // Construct the full path to the file
-        String filePath = getWorkingDir() + File.separator + proj + File.separator + currentProj.getProperty(key.toString());
-
-        Pattern p = Pattern.compile("^([a-zA-Z_0-9-/]*)_([a-z]{2})(_[A-Z]{2})?.([a-z]*)$");
-        Matcher m = p.matcher(key.toString());
-        String crowdinKey = m.matches() ? m.group(1).concat(".properties") : key.toString();
-
-        CrowdinFile master = getFactory().prepareCrowdinFile(filePath, crowdinKey, baseDir);
-        uploadTranslation(master, filePath);
-      }
-      getLog().info("Finished project " + proj);
-    }
   }
 
-  private void uploadTranslation(CrowdinFile master, String transFilePath) {
-    String transName = transFilePath.substring(transFilePath.lastIndexOf("/") + 1);
-    if (getLog().isDebugEnabled()) getLog().debug("*** Initializing: " + transName);
-    try {
-      if (getLog().isDebugEnabled()) getLog().debug("*** Upload translation: " + transName + "\n\t***** for master: " + master.getName());
-      CrowdinTranslation cTran = getFactory().prepareCrowdinTranslation(master, new File(transFilePath));
-      if (getLog().isDebugEnabled()) {
-        getLog().debug("=============================================================================");
-        getLog().debug(printFileContent(cTran.getFile()));
-        getLog().debug("=============================================================================");
+  private void uploadTranslation(CrowdinFile master) {
+    
+    if (getLog().isDebugEnabled())
+      getLog().debug("*** Init dir");
+    initDir(master.getCrowdinPath());
+
+    try {          
+      // escape special character before sync
+      FileUtils.replaceCharactersInFile(master.getFile().getPath(),
+                                        "config/special_character_processing.properties",
+                                        "EscapeSpecialCharactersBeforeSyncFromCodeToCrowdin");
+      
+      String result;
+      // check if master file already existed on Crowdin, if not then add new one 
+      if (!getHelper().elementExists(master.getCrowdinPath())) {
+        if (getLog().isDebugEnabled())
+          getLog().debug("*** Add file: " + master.getCrowdinPath());
+        result = getHelper().addFile(master);
+      }else {
+        if (getLog().isDebugEnabled()) {
+          getLog().debug("*** Update file: " + master.getCrowdinPath());
+        }
+        result = getHelper().updateFile(master);
       }
-      String result = getHelper().uploadTranslation(cTran);
-      if (result.contains("success")) getLog().info("Translation '" + transName + "' added succesfully.");
-      else getLog().warn("Cannot upload translation '" + transFilePath + " with lang '" + cTran.getLang() + "'. Reason:\n" + result);
-      if (cTran.isShouldBeCleaned()) {
-        cTran.getFile().delete();
+      // Initialize translations for the given master file
+      if (result.contains("success")) {
+        getLog().info("File " + master.getFile().getName() + " created succesfully.");
+        initTranslations(master);
+      } else {
+        getLog().warn("Cannot upload file '" + master.getFile().getPath() + "'. Reason:\n"
+            + result);
       }
+      
+      if (master.isShouldBeCleaned()) {
+        master.getFile().delete();
+      } else {
+        // remove escape special character after sync
+        FileUtils.replaceCharactersInFile(master.getFile().getPath(),
+                                          "config/special_character_processing.properties",
+                                          "EscapeSpecialCharactersAfterSyncFromCodeToCrowdin");
+      }
+    
     } catch (MojoExecutionException e) {
-      getLog().error("Error while adding translation '" + transFilePath + "'. Exception:\n" + e.getMessage());
+      getLog().error("Error while updating file '" + master.getFile().getPath()
+          + "'. Exception:\n" + e.getMessage());
     }
+    
   }
 
 }
